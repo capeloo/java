f1416448effff3f99413e0bfba5260e6c5d57b75
Caio Henrique Capêlo <108996602+capeloo@users.noreply.github.com>
2024-11-28 01:19:21 -0300

    up

---
+++ ex02_jdbc_dao/src/main/java/com/mycompany/ex02_jdbc_dao/Ex02_jdbc_dao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex02_jdbc_dao;
+
+/**
+ *
+ * @author caioc
+ */
+public class Ex02_jdbc_dao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
