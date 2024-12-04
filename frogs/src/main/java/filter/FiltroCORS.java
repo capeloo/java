3a8c0106017db15f6c7a29d2f3d2ac77b9dc0c14
Caio Henrique Capêlo <108996602+capeloo@users.noreply.github.com>
2024-12-03 21:43:16 -0300

    up

--- frogs/src/main/java/filter/FiltroCORS.java
+++ frogs/src/main/java/filter/FiltroCORS.java
@@ -22,7 +22,7 @@ public class FiltroCORS implements Filter {
         
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         
-        httpResponse.setHeader("Acess-Control-Allow-Origin", "http://localhost:4200");
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         
